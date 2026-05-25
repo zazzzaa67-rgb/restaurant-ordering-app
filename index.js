051900fcb3976c3207ff2ada37a6bd9a116804fc
zazzzaa67-rgb <z.azz.z.aa.67@gmail.com>
2026-05-25 14:32:21 +0300

    commit changes

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import  menuArray from './data.js'
 const foodKinds = document.getElementById("foodKinds")
 let html = []
-function render(arr){
+function renderkinds(arr){
     arr.map(item =>{
         html.push(`
         <div class="item">
-            <div class="emoji">${item.emoji}</div>
-            <div>
-                <h2>${item.name}</h2>
-                <p>${item.ingredients}</p>
-                <p class="price">$${item.price}</p>
+            <div class="food">
+                <div class="emoji">${item.emoji}</div>
+                <div>
+                    <h2>${item.name}</h2>
+                    <p>${item.ingredients}</p>
+                    <p class="price">$${item.price}</p>
+                </div>
             </div>
+            <button class="add" id="${item.id}"><i class="fa-solid fa-plus"></i></button>
         </div>
         
         `)
@@ -18,6 +21,23 @@ function render(arr){
     } )
     foodKinds.innerHTML = html.join(" ")
     
-
 }
-render(menuArray)
+
+function addOrder(add){
+    const orders = []
+    if (Number(add) === 0){
+        orders.push(
+            `
+                <p id="order"></p>
+            `
+        )
+    }
+}
+    
+
+
+
+renderkinds(menuArray)
+document.addEventListener("click" , function(e){
+
+})
